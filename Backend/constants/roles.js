8052d81dd3fb43d15527468a2ab6de94e70a7afa
satyamhacker <satyamgrandmaster@gmail.com>
2025-04-16 09:05:13 +0530

    added categoryController.js file in controller

---
+++ Backend/constants/roles.js
@@ -0,0 +1,11 @@
+/**
+ * System roles constants
+ * Usage: 
+ * import ROLES from './roles.js';
+ * if (user.role === ROLES.ADMIN) { ... }
+ */
+export default  {
+    CUSTOMER: 'customer',
+    RETAILER: 'retailer',
+    ADMIN: 'admin'
+  };
