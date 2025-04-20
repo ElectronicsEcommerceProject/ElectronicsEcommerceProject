29ccbf23a49df79f5ee0bfa9a844edb2eda1d2bb
Rohit Grandmaster <hyperhackinggrandmaster@gmail.com>
2025-04-20 14:15:58 +0530

    rohtiFrontend commit test

---
+++ Frontend/src/index.js
@@ -0,0 +1,19 @@
+
+import { FaEye, FaEyeSlash, FaEnvelope, FaLock, FaUser } from 'react-icons/fa';
+
+import LoginForm from "../features/auth/LoginForm";
+import SignupForm from "../features/auth/SignupForm";
+import ForgotPassword from "../features/auth/ForgotPassword"; 
+import Spinner from "../components/Loader/Spinner";
+   
+
+
+
+
+
+
+
+
+
+
+export {LoginForm,SignupForm,ForgotPassword,Spinner,FaEye,FaEyeSlash,FaEnvelope,FaLock,FaUser};
