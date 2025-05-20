6de11e94949463c2b38c6369114fd02e90cf2529
satyam Singh <55867907+satyamhacker@users.noreply.github.com>
2025-05-20 19:04:56 +0530

    Delete Backend directory

--- Backend/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-import ROLES from '../constants/roles.js';
-
-export const isAdmin = (req, res, next) => {
-  if (req.user.role !== ROLES.ADMIN) {
-    return res.status(403).json({ message: 'Admin access required' });
-  }
-  next();
-};
