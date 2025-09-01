8520d6d44f96b2ce728ab322903262728a64b6f7
satyamhacker <satyamgrandmaster@gmail.com>
2025-09-01 19:10:11 +0530

    refactor(Frontend): move URL constants to separate config file
    - Created new file 'urls/config.urls.js' to store all endpoint constants
    - Replaced direct .env imports with named exports from the new config file
    - Updated import statements in 'index.js' to use the new config file
    - This change improves code maintainability and readability by centralizing URL constants

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -51,133 +51,66 @@ import {
   getUserRole,
 } from "./utils/auth.js";
 
-//import .env routes
-const adminDashboardDataRoute = import.meta.env
-  .VITE_ADMIN_DASHBOARD_DATA_ENDPOINT;
-
-const adminProductManagementDashboardDataRoute = import.meta.env
-  .VITE_PRODUCT_MANAGMENT_DASHBOARD_DATA_ENDPOINT;
-
-//Admin Banner Management Routes
-const adminBannerRoute = import.meta.env.VITE_ADMIN_BANNER_ENDPOINT;
-const userBannerRoute = import.meta.env.VITE_USER_BANNER_ENDPOINT;
-
-const allUserRoute = import.meta.env.VITE_USER_ENDPOINT;
-const allCustomerRoute = import.meta.env.VITE_CUSTOMER_ENDPOINT;
-const allRetailerRoute = import.meta.env.VITE_RETAILER_ENDPOINT;
-
-// Cart endpoints
-const cartRoute = import.meta.env.VITE_CART_ENDPOINT;
-const cartItemRoute = import.meta.env.VITE_CART_ITEM_ENDPOINT;
-const cartItemFindOrCreateRoute = import.meta.env
-  .VITE_CART_ITEM_FIND_OR_CREATE_ENDPOINT;
-const totalCartItemNumberRoute = import.meta.env
-  .VITE_TOTAL_CART_ITEM_NUMBER_ENDPOINT;
-
-const orderRoute = import.meta.env.VITE_ORDER_ENDPOINT;
-const cancelOrderRoute = import.meta.env.VITE_ORDER_CANCEL_ENDPOINT;
-const orderItemRoute = import.meta.env.VITE_ORDER_ITEM_ENDPOINT;
-const orderItemByOrderIdRoute = import.meta.env
-  .VITE_ORDER_ITEM_ORDER_ID_ENDPOINT;
-
-const latestOrderRoute = import.meta.env.VITE_LATEST_ORDER_ENDPOINT;
-const userManagmentDashboardDataRoute = import.meta.env
-  .VITE_USER_MANAGMENT_DASHBOARD_DATA_ENDPOINT;
-
-const userManagmentDashboardUsersOrdersDataRoute = import.meta.env
-  .VITE_USER_MANAGMENT_DASHBOARD_USER_ORDERS_DATA_ENDPOINT;
-
-const reviewManagmentDashboardDataRoute = import.meta.env
-  .VITE_REVIEW_MANAGMENT_DASHBOARD_DATA_ENDPOINT;
-
-const reviewChangeStatusRoute = import.meta.env
-  .VITE_REVIEW_CHANGE_STATUS_ENDPOINT;
-
-const deleteReviewByProductReviewIdRoute = import.meta.env
-  .VITE_DELETE_REVIEW_BY_PRODUCT_REVIEW_ID_ENDPOINT;
-
-const updateProductReviewByIdRoute = import.meta.env
-  .VITE_UPDATE_PRODUCT_REVIEW_BY_ID_ENDPOINT;
-
-const reviewManagmentAnalyticsDataRoute = import.meta.env
-  .VITE_REVIEW_MANAGMENT_ANALYTICS_DASHBOARD_DATA_ENDPOINT;
-
-const couponAndOffersDashboardDataRoute = import.meta.env
-  .VITE_COUPON_AND_OFFERS_DASHBOARD_DATA_ENDPOINT;
-
-const couponAndOffersDashboardChangeStatusRoute = import.meta.env
-  .VITE_COUPON_AND_OFFERS_DASHBOARD_CHANGE_COUPON_STATUS_ENDPOINT;
-
-const getAllProductsRoute = import.meta.env.VITE_PRODUCT_ENDPOINT;
-const getAllCategoryRoute = import.meta.env.VITE_CATEGORIES_ENDPOINT;
-const getAllBrandsRoute = import.meta.env.VITE_BRANDS_ENDPOINT;
-const getBrandsByCategoryRoute = import.meta.env.VITE_BRANDS_ENDPOINT; // Will use with getApiById
-const getProductsByBrandRoute = import.meta.env.VITE_PRODUCT_BY_BRAND_ENDPOINT;
-
-const adminProductCatalogByCategoryAndBrandIdRoute = import.meta.env
-  .VITE_ADMIN_PRODUCT_CATALOG_BY_CATEGORY_AND_BRAND_ID_ENDPOINT;
-
-const productVariantByProductIdRoute = import.meta.env
-  .VITE_PRODUCT_VARIANT_BY_PRODUCT_ID_ENDPOINT;
-
-const couponAndOffersDashboardAnalyticsDataRoute = import.meta.env
-  .VITE_COUPON_AND_OFFERS_DASHBOARD_ANALYTICS_DATA_ENDPOINT;
-
-const adminReportsAnalyticsDashboardDataRoute = import.meta.env
-  .VITE_ADMIN_REPORT_ANALYTICS_DASHBOARD_DATA_ENDPOINT;
-
-const adminReportsAnalyticsProductsDataRoute = import.meta.env
-  .VITE_ADMIN_REPORT_ANALYTICS_PRODUCTS_DATA_ENDPOINT;
-
-const adminReportsAnalyticsCouponsDataRoute = import.meta.env
-  .VITE_ADMIN_REPORT_ANALYTICS_COUPONS_DATA_ENDPOINT;
-
-// admin Stock Management Routes
-const adminStockManagementVariantsRoute = import.meta.env
-  .VITE_STOCK_MANAGEMENT_VARIANTS_ENDPOINT;
-const adminStockManagementVariantByIdRoute = import.meta.env
-  .VITE_STOCK_MANAGEMENT_VARIANT_BY_ID_ENDPOINT;
-const adminStockManagementAnalyticsRoute = import.meta.env
-  .VITE_STOCK_MANAGEMENT_ANALYTICS_ENDPOINT;
-
-// Notification Management Routes
-const adminNotificationRoute = import.meta.env.VITE_ADMIN_NOTIFICATION_ENDPOINT;
-const adminNotificationAddRoute = import.meta.env
-  .VITE_ADMIN_NOTIFICATION_ADD_ENDPOINT;
-const adminNotificationLogsRoute = import.meta.env
-  .VITE_ADMIN_NOTIFICATION_LOGS_ENDPOINT;
-const adminNotificationStatsRoute = import.meta.env
-  .VITE_ADMIN_NOTIFICATION_STATS_ENDPOINT;
-const adminNotificationTemplatesRoute = import.meta.env
-  .VITE_ADMIN_NOTIFICATION_TEMPLATES_ENDPOINT;
-
-const userNotificationRoute = import.meta.env.VITE_USER_NOTIFICATION_ENDPOINT;
-const userTotalNumberOfUnReadNotificationsRoute = import.meta.env
-  .VITE_USER_TOTAL_NUMBER_OF_UNREAD_NOTIFICATIONS_ENDPOINT;
-
-//user Panel Routes
-
-const userPanelLoginRoute = import.meta.env.VITE_AUTH_LOGIN_ENDPOINT;
-const userPanelRegisterRoute = import.meta.env.VITE_AUTH_SIGNUP_ENDPOINT;
-const userPanelProductByIdDetailsRoute = import.meta.env
-  .VITE_USER_PRODUCT_BY_ID_DETAILS_ENDPOINT;
-const userPanelForgotPasswordRoute = import.meta.env
-  .VITE_USER_FORGOT_PASSWORD_ENDPOINT;
-const resetPasswordRoute = import.meta.env.VITE_RESET_PASSWORD_ENDPOINT;
-
-const userAddressesRoute = import.meta.env.VITE_ADDRESSES_ENDPOINT;
-const userProfileRoute = import.meta.env.VITE_PROFILE_ENDPOINT;
-const userDashboardDataRoute = import.meta.env
-  .VITE_USER_DASHBOARD_DATA_ENDPOINT;
-
-const userProductsByCategoryId = import.meta.env
-  .VITE_PRODUCT_BY_CATEGORY_ENDPOINT;
-
-//user coupon realted routes
-const userCouponUserRoute = import.meta.env.VITE_USER_COUPON_ENDPOINT;
-
-// user review related routes
-const userProductReviewRoute = import.meta.env.VITE_PRODUCT_REVIEW_ENDPOINT;
+// Import URL constants from config
+import {
+  ADMIN_DASHBOARD_DATA_ENDPOINT as adminDashboardDataRoute,
+  PRODUCT_MANAGMENT_DASHBOARD_DATA_ENDPOINT as adminProductManagementDashboardDataRoute,
+  ADMIN_BANNER_ENDPOINT as adminBannerRoute,
+  USER_BANNER_ENDPOINT as userBannerRoute,
+  USER_ENDPOINT as allUserRoute,
+  CUSTOMER_ENDPOINT as allCustomerRoute,
+  RETAILER_ENDPOINT as allRetailerRoute,
+  CART_ENDPOINT as cartRoute,
+  CART_ITEM_ENDPOINT as cartItemRoute,
+  CART_ITEM_FIND_OR_CREATE_ENDPOINT as cartItemFindOrCreateRoute,
+  TOTAL_CART_ITEM_NUMBER_ENDPOINT as totalCartItemNumberRoute,
+  ORDER_ENDPOINT as orderRoute,
+  ORDER_CANCEL_ENDPOINT as cancelOrderRoute,
+  ORDER_ITEM_ENDPOINT as orderItemRoute,
+  ORDER_ITEM_ORDER_ID_ENDPOINT as orderItemByOrderIdRoute,
+  LATEST_ORDER_ENDPOINT as latestOrderRoute,
+  USER_MANAGMENT_DASHBOARD_DATA_ENDPOINT as userManagmentDashboardDataRoute,
+  USER_MANAGMENT_DASHBOARD_USER_ORDERS_DATA_ENDPOINT as userManagmentDashboardUsersOrdersDataRoute,
+  REVIEW_MANAGMENT_DASHBOARD_DATA_ENDPOINT as reviewManagmentDashboardDataRoute,
+  REVIEW_CHANGE_STATUS_ENDPOINT as reviewChangeStatusRoute,
+  DELETE_REVIEW_BY_PRODUCT_REVIEW_ID_ENDPOINT as deleteReviewByProductReviewIdRoute,
+  UPDATE_PRODUCT_REVIEW_BY_ID_ENDPOINT as updateProductReviewByIdRoute,
+  REVIEW_MANAGMENT_ANALYTICS_DASHBOARD_DATA_ENDPOINT as reviewManagmentAnalyticsDataRoute,
+  COUPON_AND_OFFERS_DASHBOARD_DATA_ENDPOINT as couponAndOffersDashboardDataRoute,
+  COUPON_AND_OFFERS_DASHBOARD_CHANGE_COUPON_STATUS_ENDPOINT as couponAndOffersDashboardChangeStatusRoute,
+  PRODUCT_ENDPOINT as getAllProductsRoute,
+  CATEGORIES_ENDPOINT as getAllCategoryRoute,
+  BRANDS_ENDPOINT as getAllBrandsRoute,
+  BRANDS_ENDPOINT as getBrandsByCategoryRoute,
+  PRODUCT_BY_BRAND_ENDPOINT as getProductsByBrandRoute,
+  ADMIN_PRODUCT_CATALOG_BY_CATEGORY_AND_BRAND_ID_ENDPOINT as adminProductCatalogByCategoryAndBrandIdRoute,
+  PRODUCT_VARIANT_BY_PRODUCT_ID_ENDPOINT as productVariantByProductIdRoute,
+  COUPON_AND_OFFERS_DASHBOARD_ANALYTICS_DATA_ENDPOINT as couponAndOffersDashboardAnalyticsDataRoute,
+  ADMIN_REPORT_ANALYTICS_DASHBOARD_DATA_ENDPOINT as adminReportsAnalyticsDashboardDataRoute,
+  ADMIN_REPORT_ANALYTICS_PRODUCTS_DATA_ENDPOINT as adminReportsAnalyticsProductsDataRoute,
+  ADMIN_REPORT_ANALYTICS_COUPONS_DATA_ENDPOINT as adminReportsAnalyticsCouponsDataRoute,
+  STOCK_MANAGEMENT_VARIANTS_ENDPOINT as adminStockManagementVariantsRoute,
+  STOCK_MANAGEMENT_VARIANT_BY_ID_ENDPOINT as adminStockManagementVariantByIdRoute,
+  STOCK_MANAGEMENT_ANALYTICS_ENDPOINT as adminStockManagementAnalyticsRoute,
+  ADMIN_NOTIFICATION_ENDPOINT as adminNotificationRoute,
+  ADMIN_NOTIFICATION_ADD_ENDPOINT as adminNotificationAddRoute,
+  ADMIN_NOTIFICATION_LOGS_ENDPOINT as adminNotificationLogsRoute,
+  ADMIN_NOTIFICATION_STATS_ENDPOINT as adminNotificationStatsRoute,
+  ADMIN_NOTIFICATION_TEMPLATES_ENDPOINT as adminNotificationTemplatesRoute,
+  USER_NOTIFICATION_ENDPOINT as userNotificationRoute,
+  USER_TOTAL_NUMBER_OF_UNREAD_NOTIFICATIONS_ENDPOINT as userTotalNumberOfUnReadNotificationsRoute,
+  AUTH_LOGIN_ENDPOINT as userPanelLoginRoute,
+  AUTH_SIGNUP_ENDPOINT as userPanelRegisterRoute,
+  USER_PRODUCT_BY_ID_DETAILS_ENDPOINT as userPanelProductByIdDetailsRoute,
+  USER_FORGOT_PASSWORD_ENDPOINT as userPanelForgotPasswordRoute,
+  RESET_PASSWORD_ENDPOINT as resetPasswordRoute,
+  ADDRESSES_ENDPOINT as userAddressesRoute,
+  PROFILE_ENDPOINT as userProfileRoute,
+  USER_DASHBOARD_DATA_ENDPOINT as userDashboardDataRoute,
+  PRODUCT_BY_CATEGORY_ENDPOINT as userProductsByCategoryId,
+  USER_COUPON_ENDPOINT as userCouponUserRoute,
+  PRODUCT_REVIEW_ENDPOINT as userProductReviewRoute,
+} from "./urls/config.urls.js";
 
 export {
   FaEye,
